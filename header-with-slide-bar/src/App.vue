ed3d24376cc40b61feef50d5f63ce7d2c859ca98
Tangjj1996 <Tangjj_1996@163.com>
2020-06-25 21:01:45 +0800

    feat: :headphones: header with slide bar animation

---
+++ header-with-slide-bar/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <h1 class="title slide-bar">I'm alphardex</h1>
+    <p class="subtitle slide-bar">A CSS Wizard</p>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
