11de3e2efbd82a87c2aba82c4974a6c7585003f7
Tangjj1996 <Tangjj_1996@163.com>
2020-06-25 21:49:09 +0800

    feat: :alien: custom input

---
+++ custom-input/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <form>
+    <fieldset class="todo-list">
+      <legend class="todo-list__title">My Special Todo List</legend>
+      <label class="todo-list__label">
+        <input type="checkbox" />
+        <i class="check"></i>
+        <span>Make awesome CSS animation</span>
+      </label>
+      <label class="todo-list__label">
+        <input type="checkbox" />
+        <i class="check"></i>
+        <span>Watch awesome bangumi</span>
+      </label>
+      <label class="todo-list__label">
+        <input type="checkbox" />
+        <i class="check"></i>
+        <span>Encounter awesome people</span>
+      </label>
+      <label class="todo-list__label">
+        <input type="checkbox" />
+        <i class="check"></i>
+        <span>Be an awesome man</span>
+      </label>
+    </fieldset>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
