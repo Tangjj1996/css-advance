6ab8f44a1533548bca920bf94bbecf42635f6e36
Tangjj1996 <Tangjj_1996@163.com>
2020-06-25 13:27:15 +0800

    feat: :snowflake: snow animation

---
+++ snow/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './src/App'
+
+import './snow.scss'
+
+new Vue({
+  el: '#root',
+  render: h => h(App)
+})
---
+++ snow/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div ref="appRef">
+    <div class="snow" v-for="item in 200" :key="Math.random()"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<style>
+</style>
